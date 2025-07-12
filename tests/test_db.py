50b53b3378fd1b3ae45b9fa24713273e8ebf14ca
LecoOliveira <lecoverde10@gmail.com>
2025-07-12 14:18:32 -0300

    Adicionando banco, mock e testes

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,19 +1,27 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from to_do_list.models import User
 
 
-def test_create_user(session):
-    new_user = User(
-        username='testuser',
-        email='test@test.com',
-        password='password123',
-    )
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='testuser',
+            email='test@test.com',
+            password='password123',
+        )
 
-    session.add(new_user)
-    session.commit()
-    user = session.scalar(select(User).where(User.username == 'testuser'))
+        session.add(new_user)
+        session.commit()
+        user = session.scalar(select(User).where(User.username == 'testuser'))
 
-    assert user.username == 'testuser'
-    assert user.email == 'test@test.com'
-    assert user.password == 'password123'
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'testuser',
+        'email': 'test@test.com',
+        'password': 'password123',
+        'created_at': time,
+        'updated_at': time,
+    }
